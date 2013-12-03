af302fa4818b2879d5eca8fbae23d7ff87c44c52
John <s.johnwatson@gmail.com>
2013-12-03 15:02:53 -0500

    made employee list csv

---
+++ main_file.rb
@@ -0,0 +1,24 @@
+#Need an Employee class
+#    tax rate is 30%
+#    need a csv to read-in a list of employees
+
+
+
+#Owner class
+#   monthly quota must hit 250,000 for owner to get 1000 bonus
+#
+
+
+
+
+#Comission Sales Person class
+
+#Quota SalesPerson Class
+
+#SaleClass
+#   Should have a class method that returns a list of sales loaded from the CSV
+#   Need to read in a csv file with the the sales data
+
+#Need to output to a textfile with the salary information
+#......(Gross pay + any bonus/comission)
+
